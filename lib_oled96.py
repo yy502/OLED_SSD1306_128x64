0cec16585d9db2bb545c08f8ca0700e7f666dfd4
Yi Yu <yi.yu@ocado.com>
2017-01-05 15:12:21 +0000

    tidy up

--- lib_oled96.py
+++ lib_oled96.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python
 
-# Substantially derived from code by (c) 2015 Richard Hull The MIT License (MIT)
-# https://github.com/rm-hull/ssd1306:
-#       "Permission is hereby granted, free of charge, to any person obtaining a copy
-#       "of this software and associated documentation files (the "Software"), to deal
-#       "in the Software without restriction, including without limitation the rights
-#       "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#       "copies of the Software, and to permit persons to whom the Software is
-#       "furnished to do so, subject to the following conditions:
-#       "The above copyright notice and this permission notice shall be included in
-#       "all copies or substantial portions of the Software."
-#
-# B Lavery 2015:
-# This derivative "library" module is not installed to the python system as Hull's version was:
-# it simply resides alongside your own python script.
-# In this version, the I2C bus object needs to be handed in as a parameter from your user code.
-# This makes this one same library file work with either Raspberry Pi or Virtual GPIO system.
-# Hull's (clever) auto-displaying "canvas" is replaced by a persistent draw object
-# which can be incrementally changed. This canvas needs coded "display()" calls to push to the hardware.
-
 from PIL import Image, ImageDraw
 
+class Const:
+    CHARGEPUMP = 0x8D
+    COLUMNADDR = 0x21
+    COMSCANDEC = 0xC8
+    COMSCANINC = 0xC0
+    DISPLAYALLON = 0xA5
+    DISPLAYALLON_RESUME = 0xA4
+    DISPLAYOFF = 0xAE
+    DISPLAYON = 0xAF
+    EXTERNALVCC = 0x1
+    INVERTDISPLAY = 0xA7
+    MEMORYMODE = 0x20
+    NORMALDISPLAY = 0xA6
+    PAGEADDR = 0x22
+    SEGREMAP = 0xA0
+    SETCOMPINS = 0xDA
+    SETCONTRAST = 0x81
+    SETDISPLAYCLOCKDIV = 0xD5
+    SETDISPLAYOFFSET = 0xD3
+    SETHIGHCOLUMN = 0x10
+    SETLOWCOLUMN = 0x00
+    SETMULTIPLEX = 0xA8
+    SETPRECHARGE = 0xD9
+    SETSEGMENTREMAP = 0xA1
+    SETSTARTLINE = 0x40
+    SETVCOMDETECT = 0xDB
+    SWITCHCAPVCC = 0x2
 
 class ssd1306():
 
@@ -36,22 +44,22 @@ class ssd1306():
         self.canvas = ImageDraw.Draw(self.image) # this is a "draw" object for preparing display contents
 
         self._command(
-            const.DISPLAYOFF,
-            const.SETDISPLAYCLOCKDIV, 0x80,
-            const.SETMULTIPLEX,       0x3F,
-            const.SETDISPLAYOFFSET,   0x00,
-            const.SETSTARTLINE,
-            const.CHARGEPUMP,         0x14,
-            const.MEMORYMODE,         0x00,
-            const.SEGREMAP,
-            const.COMSCANDEC,
-            const.SETCOMPINS,         0x12,
-            const.SETCONTRAST,        0xCF,
-            const.SETPRECHARGE,       0xF1,
-            const.SETVCOMDETECT,      0x40,
-            const.DISPLAYALLON_RESUME,
-            const.NORMALDISPLAY,
-            const.DISPLAYON)
+            Const.DISPLAYOFF,
+            Const.SETDISPLAYCLOCKDIV, 0x80,
+            Const.SETMULTIPLEX,       0x3F,
+            Const.SETDISPLAYOFFSET,   0x00,
+            Const.SETSTARTLINE,
+            Const.CHARGEPUMP,         0x14,
+            Const.MEMORYMODE,         0x00,
+            Const.SEGREMAP,
+            Const.COMSCANDEC,
+            Const.SETCOMPINS,         0x12,
+            Const.SETCONTRAST,        0xCF,
+            Const.SETPRECHARGE,       0xF1,
+            Const.SETVCOMDETECT,      0x40,
+            Const.DISPLAYALLON_RESUME,
+            Const.NORMALDISPLAY,
+            Const.DISPLAYON)
 
     def _command(self, *cmd):
         """
@@ -81,22 +89,21 @@ class ssd1306():
         """
 
         self._command(
-            const.COLUMNADDR, 0x00, self.width-1,  # Column start/end address
-            const.PAGEADDR,   0x00, self.pages-1)  # Page start/end address
+            Const.COLUMNADDR, 0x00, self.width-1,  # Column start/end address
+            Const.PAGEADDR,   0x00, self.pages-1)  # Page start/end address
 
         pix = list(self.image.getdata())
-        step = self.width * 8
         buf = []
-        for y in range(0, self.pages * step, step):
-            i = y + self.width-1
-            while i >= y:
-                byte = 0
-                for n in range(0, step, self.width):
-                    byte |= (pix[i + n] & 0x01) << 8
-                    byte >>= 1
 
-                buf.append(byte)
-                i -= 1
+        def make_byte(*lines):
+            # Even though self.image is B/W, the pixel value is sometimes set to 255 instead of 1
+            return ((lines[7] & 1) << 7 | (lines[6] & 1) << 6 | (lines[5] & 1) << 5
+                | (lines[4] & 1) << 4 | (lines[3] & 1) << 3 | (lines[2] & 1) << 2
+                | (lines[1] & 1) << 1 | (lines[0] & 1))
+
+        lines = [ pix[i:i + self.width] for i in xrange(0, self.width * self.height, self.width) ]
+        for l in range(0, self.height, 8):
+            buf.extend(reversed(map(make_byte, *lines[l:l + 8])))
 
         self._data(buf) # push out the whole lot
 
@@ -105,36 +112,8 @@ class ssd1306():
         self.display()
 
     def onoff(self, onoff):
-        if onoff == 0:
-            self._command(const.DISPLAYOFF)
+        if onoff:
+            self._command(Const.DISPLAYON)
         else:
-            self._command(const.DISPLAYON)
+            self._command(Const.DISPLAYOFF)
 
-
-class const:
-    CHARGEPUMP = 0x8D
-    COLUMNADDR = 0x21
-    COMSCANDEC = 0xC8
-    COMSCANINC = 0xC0
-    DISPLAYALLON = 0xA5
-    DISPLAYALLON_RESUME = 0xA4
-    DISPLAYOFF = 0xAE
-    DISPLAYON = 0xAF
-    EXTERNALVCC = 0x1
-    INVERTDISPLAY = 0xA7
-    MEMORYMODE = 0x20
-    NORMALDISPLAY = 0xA6
-    PAGEADDR = 0x22
-    SEGREMAP = 0xA0
-    SETCOMPINS = 0xDA
-    SETCONTRAST = 0x81
-    SETDISPLAYCLOCKDIV = 0xD5
-    SETDISPLAYOFFSET = 0xD3
-    SETHIGHCOLUMN = 0x10
-    SETLOWCOLUMN = 0x00
-    SETMULTIPLEX = 0xA8
-    SETPRECHARGE = 0xD9
-    SETSEGMENTREMAP = 0xA1
-    SETSTARTLINE = 0x40
-    SETVCOMDETECT = 0xDB
-    SWITCHCAPVCC = 0x2
